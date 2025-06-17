1ffa7d6eed7fe0bd8994db5ea30f7f34846a3205
Leidychavez8 <leidychor08@gmail.com>
2025-06-17 09:14:52 -0500

    mi primer commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\postController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomeController::class );
+
+Route:: get('/posts', [postController::class, 'index']);
+
+Route:: get('/posts/create', [postController::class, 'create']);
+
+Route:: get('/posts-/{post}', [postController::class,'show']);
+
+/*Route:: get('/posts-/{post}/{category}', function ($post, $category){
+return "aqui se mostrara post el {$post} de la categoria {$category}";
+});*/
+
